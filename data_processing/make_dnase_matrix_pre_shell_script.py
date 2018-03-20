c27a202cc3513392923cbec16e333c7f612e7965
Cici Chen <wychen66@alumni.stanford.edu>
2018-03-20 11:40:45 -0700

    added make_dnase_matrix_pre_shell_script.py

--- data_processing/make_dnase_matrix_pre_shell_script.py
+++ data_processing/make_dnase_matrix_pre_shell_script.py
@@ -3,61 +3,60 @@
 
 # In[26]:
 
-#def make_dnase_matrix_pre_shell_script(output_path):
+def make_dnase_matrix_pre_shell_script(output_path):
     # make 5 useful dnase file columns into lists
-output_path = '/srv/scratch/wychen66/get_correlation/'
-import numpy as np
-lab_list=[]
-biosample_matched_rna_list=[]
-eid_matched_rna_list=[]
-pooled_tagAlign_list=[]
-idr_file_list=[]
-cell_list=[]
-color_list=[]
-dnase_folder_name_list=[]
+    import numpy as np
+    lab_list=[]
+    biosample_matched_rna_list=[]
+    eid_matched_rna_list=[]
+    pooled_tagAlign_list=[]
+    idr_file_list=[]
+    cell_list=[]
+    color_list=[]
+    dnase_folder_name_list=[]
 
-with open('/mnt/data/integrative/metadata/dnase_metadata_2016-12-05.txt','r') as f_txt: 
-    for line in f_txt:
-        if 'idr_file' in line: # skip over header line
-            continue
-        items = line.split('\t') # 431 lines total
-        if 'UW_Stam' not in items[23]:
-            continue
-        if items[17] is not "":
-            lab_list.append(items[23])
-            color_list.append(items[22])
-            biosample_matched_rna_list.append(items[17])
-            dnase_folder_name_list.append(items[0])
-            eid_matched_rna_list.append(items[18])
-            pooled_tagAlign_list.append(items[6])
-            idr_file_list.append(items[9])
-            cell_list.append(items[20])
+    with open('/mnt/data/integrative/metadata/dnase_metadata_2016-12-05.txt','r') as f_txt: 
+        for line in f_txt:
+            if 'idr_file' in line: # skip over header line
+                continue
+            items = line.split('\t') # 431 lines total
+            if 'UW_Stam' not in items[23]:
+                continue
+            if items[17] is not "":
+                lab_list.append(items[23])
+                color_list.append(items[22])
+                biosample_matched_rna_list.append(items[17])
+                dnase_folder_name_list.append(items[0])
+                eid_matched_rna_list.append(items[18])
+                pooled_tagAlign_list.append(items[6])
+                idr_file_list.append(items[9])
+                cell_list.append(items[20])
 
-biosample_matched_rna = np.asarray(biosample_matched_rna_list)
-eid_matched_rna = np.asarray(eid_matched_rna_list)
-pooled_tagAlign = np.asarray(pooled_tagAlign_list)
-idr_file = np.asarray(idr_file_list)
-color = np.asarray(color_list)
-dnase_folder_name = np.asarray(dnase_folder_name_list)
-pca_labels_final = cell_list
+    biosample_matched_rna = np.asarray(biosample_matched_rna_list)
+    eid_matched_rna = np.asarray(eid_matched_rna_list)
+    pooled_tagAlign = np.asarray(pooled_tagAlign_list)
+    idr_file = np.asarray(idr_file_list)
+    color = np.asarray(color_list)
+    dnase_folder_name = np.asarray(dnase_folder_name_list)
+    pca_labels_final = cell_list
 
-bios_eid_tag_idr_color_folder = np.stack((biosample_matched_rna,eid_matched_rna,pooled_tagAlign,idr_file,color,dnase_folder_name))
-bios_eid_tag_idr_color_folder = bios_eid_tag_idr_color_folder.transpose()
-f_txt.close()
+    bios_eid_tag_idr_color_folder = np.stack((biosample_matched_rna,eid_matched_rna,pooled_tagAlign,idr_file,color,dnase_folder_name))
+    bios_eid_tag_idr_color_folder = bios_eid_tag_idr_color_folder.transpose()
+    f_txt.close()
 
-idr_files = bios_eid_tag_idr_color_folder[:,3] # make bed file of the idr_files to manipulation in shell script
-tag_files = bios_eid_tag_idr_color_folder[:,2]
-bios_eid_list = bios_eid_tag_idr_color_folder[:,0:2]
-pca_color = bios_eid_tag_idr_color_folder[:,4]
-dnase_folder_name = bios_eid_tag_idr_color_folder[:,5]
+    idr_files = bios_eid_tag_idr_color_folder[:,3] # make bed file of the idr_files to manipulation in shell script
+    tag_files = bios_eid_tag_idr_color_folder[:,2]
+    bios_eid_list = bios_eid_tag_idr_color_folder[:,0:2]
+    pca_color = bios_eid_tag_idr_color_folder[:,4]
+    dnase_folder_name = bios_eid_tag_idr_color_folder[:,5]
 
-np.savetxt(output_path + 'idr_files.bed', idr_files, fmt="%s", delimiter='\t')
-np.savetxt(output_path + 'tag_files.bed', tag_files, fmt="%s", delimiter='\t') # this is the column labels
-np.savetxt(output_path + 'bios_eid_for_final_dnase_matrix.txt', bios_eid_list, fmt="%s", delimiter='\t')
-np.savetxt(output_path + 'pca_labels.txt', pca_labels_final, fmt="%s", delimiter='\t')
-np.savetxt(output_path + 'pca_color_codes.txt', list(pca_color), fmt="%s", delimiter='\t')
-np.savetxt(output_path + 'folder_name.txt', list(dnase_folder_name), fmt="%s", delimiter='\t')
-'''
+    np.savetxt(output_path + 'idr_files.bed', idr_files, fmt="%s", delimiter='\t')
+    np.savetxt(output_path + 'tag_files.bed', tag_files, fmt="%s", delimiter='\t') # this is the column labels
+    np.savetxt(output_path + 'bios_eid_for_final_dnase_matrix.txt', bios_eid_list, fmt="%s", delimiter='\t')
+    np.savetxt(output_path + 'pca_labels.txt', pca_labels_final, fmt="%s", delimiter='\t')
+    np.savetxt(output_path + 'pca_color_codes.txt', list(pca_color), fmt="%s", delimiter='\t')
+    np.savetxt(output_path + 'folder_name.txt', list(dnase_folder_name), fmt="%s", delimiter='\t')
+
 def main():
     import argparse
     parser = argparse.ArgumentParser()
@@ -67,5 +66,4 @@ def main():
     print('Output path: ' + str(args.output_path) + 'idr_files.bed')
     
 main()
-'''
 
