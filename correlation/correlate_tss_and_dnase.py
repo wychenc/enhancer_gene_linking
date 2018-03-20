556f681431811b2d2eae04bcff2e25c73003d114
Cici Chen <wychen66@alumni.stanford.edu>
2018-03-19 21:10:25 -0700

    added correlate_tss_and_dnase.py

--- correlation/correlate_tss_and_dnase.py
+++ correlation/correlate_tss_and_dnase.py
@@ -103,10 +103,14 @@ def run_correlation_method(output_path, rna_mat_file, rna_row_labels, rna_col_la
             target = 1
             #target = [57,67,92,98,108,137]  # example points
             if counter == target:
-                global x_rna = rna_vec
-                global y_dna = dna_vec
-                global x_loc = rna_loc
-                global y_loc = dna_loc
+                global x_rna 
+                x_rna = rna_vec
+                global y_dna 
+                y_dna = dna_vec
+                global x_loc 
+                x_loc = rna_loc
+                global y_loc 
+                y_loc = dna_loc
 
             if np.std(rna_vec)==0 or np.std(dna_vec)==0: # to avoid nan's            
                 continue
